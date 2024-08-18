83b1001df97a7b5628bf8e408cca099bda8484dd
yi-del <karadagirmak99@gmail.com>
2024-08-18 16:46:57 +0300

    nasılsın

---
+++ app.js
@@ -0,0 +1,2 @@
+lorem500
+https://www.google.com/search?q=temel+js+kodalr%C4%B1&rlz=1C1CHBD_trTR1075TR1075&oq=temel+js+kodalr%C4%B1&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQABgNGIAEMgoIAhAAGIAEGKIEMgoIAxAAGIAEGKIE0gEIMjcxMWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8
